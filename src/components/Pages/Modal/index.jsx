587ae102a73e203cc7c6d0c0ceb2d65d0f427a5d
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2019-05-09 14:09:12 +0430

    Add modal

---
+++ src/components/Pages/Modal/index.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Modal from 'Root/components/Tools/Modal';
+
+import styles from './styles.less';
+
+class Modals extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <article>
+          <section className={styles.circle}>!</section>
+          <b>Delete Requset</b>
+        </article>
+        <p>Are you sure you want to delete this request?</p>
+        <div className={styles.bottom}>
+          <button type="button" className={styles.cancelButton}>Cancel</button>
+          <button type="button" className={styles.buttonDel}>Delete</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modals;
