03ac8c21ec29f33703f35fdeecb1234b7f684a27
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2019-05-26 12:39:40 +0430

    add Radiobutton

--- src/components/Tools/RadioButton/index.jsx
+++ src/components/Tools/RadioButton/index.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import styles from './styles.less';
 
-function RadioButton() {
+function RadioButton(props) {
+  const {
+    handleCheck,
+  } = props;
+
   return (
     <label className={styles.container}>
-      <input type="radio" checked="checked" name="radio" />
+      <input type="radio" defaultChecked="checked" name="radio" onClick={handleCheck} />
       <span className={styles.checkmark}></span>
     </label>
   );
