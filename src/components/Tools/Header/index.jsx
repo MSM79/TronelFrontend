31c5933cc1e5afdb9e20210827c86fbcd032a551
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2019-05-26 12:39:12 +0430

    fixed header

--- src/components/Tools/Header/index.jsx
+++ src/components/Tools/Header/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import { NavLink } from 'react-router-dom';
+import CopyText from 'Root/components/Tools/CopyText';
+
+import shorter from 'Root/helpers/shorter';
 
 import assetIcon from 'Root/images/asset.png';
 import iconsplus from 'Root/images/icons-8-plus.png';
@@ -9,15 +12,17 @@ import trx from 'Root/images/image-2019-04-20-01-21-26.png';
 import styles from './styles.less';
 
 function Header() {
+  var address = "TJWzn8rjLYbfS3hcAVVscLeERUs6rfMoA5";
+
   return (
     <header className={styles.header}>
       <div>
         <img src={assetIcon} alt="asset" className={styles.logo} />
       </div>
       <div className={styles.first}>
-        <NavLink to="/" activeClassName={styles.activeLink}>Explorer</NavLink>
-        <NavLink to="/mybets" activeClassName={styles.activeLink}>My Bets</NavLink>
-        <NavLink to="/myrequests" activeClassName={styles.activeLink}>My Requests</NavLink>
+        <NavLink exact to="/" activeClassName={styles.activeLink}>Explorer</NavLink>
+        <NavLink exact to="/mybets" activeClassName={styles.activeLink}>My Bets</NavLink>
+        <NavLink exact to="/myrequests" activeClassName={styles.activeLink}>My Requests</NavLink>
       </div>
       <div>
         <NavLink to="/createrequest">
@@ -29,10 +34,8 @@ function Header() {
       </div>
       <div className={styles.address}>
         <p>Address:</p>
-        <span>TJWzn8rjLYbfS3hcAVVscLeERUs6rfMoA5</span>
-        <a href="#">
-          <img src={iconcopy} alt="iconcopy" />
-        </a>
+        <span>{shorter(address)}</span>
+        <CopyText text={address} />
       </div>
       <div className={styles.trx}>
         <p>100 TRX</p>
