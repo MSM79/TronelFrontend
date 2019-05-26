ab8e788bc58aaf1469feb07543641d81227657b0
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2019-05-26 12:34:14 +0430

    Complete Explorer

--- src/components/Pages/Explorer/index.jsx
+++ src/components/Pages/Explorer/index.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from 'Root/components/Tools/Header';
-
 import Form from 'Root/components/Tools/Form';
 import Dropdown from 'Root/components/Tools/Dropdown';
 import Menu, { Item as MenuItem, Divider } from 'rc-menu';
-import ReactDOM from 'react-dom';
-import Slider, { Range } from 'rc-slider';
+import Modals from 'Root/components/Pages/Modal';
+import Modal from 'Root/components/Tools/Modal';
+import Slider, { createSliderWithTooltip } from 'rc-slider';
 
+import acceptIcon from 'Root/images/accept.png';
 import 'rc-slider/assets/index.css';
 import styles from './styles.less';
+
+import bitcoinIcon from 'Root/images/bitcoin.png';
+import ethereumIcon from 'Root/images/ethereum.png';
 
 function onSelect({ key }) {
   console.log(`${key} selected`);
 }
 
+const Range = createSliderWithTooltip(Slider.Range);
+
 const menu = (
-  <Menu onSelect={onSelect}>
-    <MenuItem disabled>disabled</MenuItem>
-    <MenuItem key="1">one</MenuItem>
+  <Menu onSelect={onSelect} className={styles.menu}>
+    <MenuItem key="1" className={styles.menuItem}>Bitcoin</MenuItem>
     <Divider />
-    <MenuItem key="2">two</MenuItem>
+    <MenuItem key="2" className={styles.menuItem}>Ethereum</MenuItem>
   </Menu>
 );
 
@@ -28,7 +33,27 @@ function range(e) {
   console.log(e);
 }
 
-function Explorer() {
+class Explorer extends Component {
+
+  state = {
+    visible: false,
+  }
+
+  show = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  hide = () => {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  render() {
+    const { visible } = this.state;
+
   return (
     <div>
       <Header />
@@ -43,40 +68,63 @@ function Explorer() {
             <article className={styles.amount}>
               <p>Amount of bet</p>
                 <Range onChange={range}
-                defaultValue={[20,50]}
-                railStyle={{ background:'#b2b4b7'}}
-                className={styles.range}/>
+                  defaultValue={[20,50]}
+                  railStyle={{ background:'#b2b4b7'}}
+                  className={styles.range}
+                  tipFormatter={value => `${value} TRX`}
+                  tipProps={{
+                     overlayClassName: styles.tooltip,
+                     placement: 'bottom',
+                   }}
+                />
             </article>
             <article className={styles.form}>
               <p>Form number</p>
               <div>
-                <input type="text" placeholder="Enter ypur form number" />
+                <input type="text" placeholder="Enter your form number" pattern="[A-Za-z]" />
                 <input type="submit" value="Search" />
               </div>
             </article>
           </div>
           <div className={styles.row2}>
             <Form
-              formnumber="276736719919836672"
-              requseter="WTEA674fdDe714fd979de3EdF6A…"
+              handleDelete={this.show}
+              handleClose={this.hide}
+              formnumber="WTEA674fdDe714fd979de3EdF6AAA"
+              requseter="WTEA674fdDe714fd979de3EdF6AAA"
               predictedprice="Greater than or equal $30"
               amountofbet="500 TRX"
               specifieddate="2019/05/12     12:00 UTC"
               value="Accept"
+              src={bitcoinIcon}
             />
             <Form
-              formnumber="276736719919836672"
-              requseter="WTEA674fdDe714fd979de3EdF6A…"
+              handleDelete={this.show}
+              handleClose={this.hide}
+              formnumber="WTEA674fdDe714fd979de3EdF6AAA"
+              requseter="WTEA674fdDe714fd979de3EdF6AAA"
               predictedprice="Greater than or equal $30"
               amountofbet="500 TRX"
               specifieddate="2019/05/12  |  12:00 UTC"
               value="Accept"
+              src={ethereumIcon}
             />
           </div>
         </section>
       </div>
+      <Modal width={479} height={225} visible={visible} onClose={this.hide} theme="blue">
+        <Modals
+          text="Are you sure you want to accept this request?"
+          title="Accept Requset"
+          button="Accept"
+          handleClose={this.hide}
+          icon={acceptIcon}
+         />
+      </Modal>
+      <Menu />
     </div>
   );
+  }
 }
 
 export default Explorer;
