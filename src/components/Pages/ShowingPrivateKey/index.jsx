efa23406202dd1e3256e15a49eb8e3e8bd3459ab
MohammadSeyfiMarandi <mohammadmarandi89@gmail.com>
2019-05-28 02:49:17 +0430

    add shorter && copyText

--- src/components/Pages/ShowingPrivateKey/index.jsx
+++ src/components/Pages/ShowingPrivateKey/index.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 
+import shorter from 'Root/helpers/shorter';
+import CopyText from 'Root/components/Tools/CopyText';
+
 import copyIcon from 'Root/images/icons-8-copy.png';
 
 import styles from './styles.less';
 
 function PrivateKey() {
+  var key = "TJWzn8rjLYbfS3hcAVVscLeERUs6rfMoA5";
   return (
     <section className={styles.all}>
       <h1>Showing private key</h1>
       <div className={styles.container}>
         <p>Your private key:</p>
         <div>
-          <p>TJWzn8rjLYbfS3hcAVVscLeERUs6rfMoA5</p>
-          <img src={copyIcon} alt="copyIcon" />
+          <p>{shorter(key)}</p>
+          <CopyText text={key} />
         </div>
         <input type="submit" value="Confirm" />
       </div>
